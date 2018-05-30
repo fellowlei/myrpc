4aa4418a10723de79f41cab2e0d438da0781c86e
lulei11 <lulei@jd.com>
2018-05-30 14:15:59 +0800

    init

--- service/src/main/java/com/mark/im/chat2/protocol/RpcEncoder.java
+++ service/src/main/java/com/mark/im/chat2/protocol/RpcEncoder.java
@@ -5,10 +5,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-/**
- * RPC Encoder
- * @author huangyong
- */
 public class RpcEncoder extends MessageToByteEncoder {
 
     private Class<?> targetClazz;
